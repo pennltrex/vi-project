91e266e995e7675139c48bb88f19c99a09a64aea
Aaron Milligan <abm@wsma.org>
2018-07-27 10:32:30 -0700

    Verification complained about spaces. I fixed the.

--- src/app/event.ts
+++ src/app/event.ts
@@ -2,24 +2,22 @@ export class Event {
 
     title: string;
     description: string;
-    imgUrl:string;
+    imgUrl: string;
     eventid: string;
-     beginDate:Date;
-     location:string;
-     eventPageUrl:string;
+     beginDate: Date;
+     location: string;
+     eventPageUrl: string;
 
-     constructor(title:string, desc:string, begindate:string, Image:string, eventID:string, location:string, url:string) {
+     constructor(title: string, desc: string, begindate: string, Image: string, eventID: string, location: string, url: string) {
         this.title = title;
-        this.description=desc;
-        if(Image.length>0 && Image.startsWith("~")) {
+        this.description = desc;
+        if (Image.length > 0 && Image.startsWith('~')) {
             this.imgUrl = Image.substring(1);
-        }
-        else
-        {
+        } else {
             this.imgUrl = Image;
         }
         this.beginDate = new Date(begindate);
-        this.eventid=eventID;
+        this.eventid = eventID;
         this.location = location;
         this.eventPageUrl = url;
      }
